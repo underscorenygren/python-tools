4ab33371d64cba04d6e4a0379bfb79e8ee6c4d0b
Erik Nygren <sten.erik.nygren@gmail.com>
2019-09-26 17:33:34 -0400

    addded apigatway, made argparseable

--- aws/tagger.py
+++ aws/tagger.py
@@ -8,6 +8,7 @@ class Service(object):
 	def __init__(self, service_name):
 		self.service_name = service_name
 		self.cli = boto3.client(service_name)
+		self.region_name = boto3.session.Session().region_name
 
 	def paginator(self, fn_name, **kwargs):
 		pager = self.cli.get_paginator(fn_name)
@@ -41,6 +42,42 @@ class Lambda(Service):
 			"Name": self.name(item)
 		}
 
+	def tag_it(self, arn, tags):
+		self.cli.tag_resource(Resource=arn, Tags=new_tags)
+
+
+class APIGateway(Service):
+	def __init__(self):
+		super().__init__('apigateway')
+
+	def entries(self):
+		for entry in self.paginator('get_rest_apis'):
+			yield entry
+
+	def items(self, resp):
+		return resp['items']
+
+	def _id(self, item):
+		return item['id']
+
+	def arn(self, item):
+		_id = self._id(item)
+		return f"arn:aws:apigateway:{self.region_name}::/restapis/{_id}"
+
+	def name(self, item):
+		return item['name']
+
+	def get_tags(self, item):
+		return item.get('tags', self.cli.get_tags(resourceArn=self.arn(item))['tags'])
+
+	def new_tags(self, item):
+		return {
+			"Name": self.name(item)
+		}
+
+	def tag_it(self, arn, tags):
+		self.cli.tag_resource(resourceArn=arn, tags=tags)
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -52,6 +89,7 @@ MODE_OVERWRITE = 'overwrite'
 MODE_ADD = 'add'
 # only adds tags if object non-empty
 MODE_CREATE_ONLY = 'create-only'
+MODES = [MODE_OVERWRITE, MODE_ADD, MODE_CREATE_ONLY]
 
 def _default_filterfn(svc, item):
 	return True
@@ -82,8 +120,21 @@ def tag(svc, mode=MODE_CREATE_ONLY, filterfn=_default_filterfn, dry_run=False):
 	for arn, new_tags in updates:
 		logger.debug(f"adding tags {new_tags} to {arn}")
 		if not dry_run:
-			svc.cli.tag_resource(Resource=arn, Tags=new_tags)
+			svc.tag_it(arn, new_tags)
 
 
 if __name__ == "__main__":
-	tag(Lambda(), mode=MODE_ADD, filterfn=lambda svc, i: svc.get_tags(i).get('Name') is None)
+	import argparse
+	parser = argparse.ArgumentParser(description="tags amazon resource names")
+
+	klasses = [subclass.__name__ for subclass in Service.__subclasses__()]
+
+	parser.add_argument('service', type=str, choices=klasses, help="The service to tag")
+	parser.add_argument('--dry-run', action="store_true", help="lists resources and tags, but doesn't actually perform tagging")
+	parser.add_argument('--mode', type=str, choices=MODES, default=MODE_CREATE_ONLY, help="what to do with existing tags: overwrite, add if not present, or skip if any present")
+
+	args = parser.parse_args()
+
+	cls = globals()[args.service]
+
+	tag(cls(), mode=args.mode, dry_run=args.dry_run)
